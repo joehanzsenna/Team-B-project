fa80cdfd30690bad295488dd74746e13135f1857
juliet oti <egwuotijulietchinenye@gmail.com>
2023-04-18 15:34:56 +0100

    new commit

---
+++ src/pages/dashboard/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../styles/dashboard/sidebar.css";
+import booklogo from "../../assets/logos_growth-book-icon.png";
+import windows from "../../assets/frame.png";
+import bookmark from "../../assets/book.png";
+import people from "../../assets/people.png";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container ">
+      <div className="logo-container">
+        <img src={booklogo} alt="" className="sidebar-logo" />
+      </div>
+      <div className="features-icons d-flex">
+        <img src={windows} alt="" />
+        <img src={bookmark} alt="" />
+        <img src={people} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
