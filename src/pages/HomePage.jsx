f536212f766ec199642aad4cdcf7cbbfd935db26
Joehanz_senna <jonathanedison34@gmail.com>
2023-04-18 09:35:51 -0600

    new commit on some deletes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import HomeSection1 from '../components/HomePageComponents/HomeSection1'
-import HomeSection10 from '../components/HomePageComponents/HomeSection10'
 import HomeSection2 from '../components/HomePageComponents/HomeSection2'
 import HomeSection3 from '../components/HomePageComponents/HomeSection3'
 import HomeSection4 from '../components/HomePageComponents/HomeSection4'
 import HomeSection5 from '../components/HomePageComponents/HSection5Folder/HomeSection5'
 import HomeSection6 from '../components/HomePageComponents/HomeSection6'
 import HomeSection7 from '../components/HomePageComponents/HomeSection7'
-import HomeSection8 from '../components/HomePageComponents/HomeSection8'
-import HomeSection9 from '../components/HomePageComponents/HomeSection9'
 
 const HomePage = () => {
   return (
@@ -20,9 +17,6 @@ const HomePage = () => {
       <HomeSection5/>
       <HomeSection6/>
       <HomeSection7/>
-      {/* <HomeSection8/> */}
-      {/* <HomeSection9/> */}
-      {/* <HomeSection10/> */}
       
     </div>
   )
