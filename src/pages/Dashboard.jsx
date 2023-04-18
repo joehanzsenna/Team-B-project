1158217fcd37b3259c568e76ed4de00a40e7c552
juliet oti <egwuotijulietchinenye@gmail.com>
2023-04-18 15:29:06 +0100

    new commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Section1 from '../components/DashboardComponents/Section1'
 import Section2 from '../components/DashboardComponents/Section2'
-import Sidebar from '../components/DashboardComponents/Sidebar'
-import Topbar from '../components/DashboardComponents/Topbar'
 import DashboardLayout from '../components/layout/DashboardLayout'
 
 const Dashboard = () => {
   return (
     <div className="container">
       <DashboardLayout >
-        {/* <Topbar/> */}
-        {/* <Sidebar/> */}
         <Section1/>
         <Section2/>
     </DashboardLayout>
