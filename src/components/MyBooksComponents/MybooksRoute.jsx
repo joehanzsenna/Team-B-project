17a2eea4ea34b0827fdf237a2bd0469834b681c9
Joehanz_senna <jonathanedison34@gmail.com>
2023-04-22 07:41:31 -0600

    new update route completed

--- src/components/MyBooksComponents/MybooksRoute.jsx
+++ src/components/MyBooksComponents/MybooksRoute.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
 
 const MybooksRoute = () => {
-    const [whenActive, setWhenActive] = useState(true)
+    const [whenActive, setWhenActive] = useState(0)
 
     const toggle = (index) => {
         setWhenActive(index)
@@ -11,21 +11,26 @@ const MybooksRoute = () => {
   return (
     <div>
         <div className='MybooksSection1-tab'>
-
-            <NavLink to='/mybookspage' className='linkStyle'>
-                 <li className={whenActive == 1 ? `isActive` : ''}
+        {/* className={({ isActive }) => (isActive ? active)} */}
+            <NavLink to='/mybookspage'
+                className={whenActive == 1 ? ` active` : ' normal'}
                 onClick={() => toggle(1)}
-                >All Books</li>
+            >
+                 <li className='nav-li'>All Books</li>
             </NavLink>
-            <NavLink to='/purchasedbookspage' className='linkStyle'>
-                    <li className={whenActive == 2 ? `isActive` : ''}
-                    onClick={() => toggle(2)}
-                    >PurchasedBooks</li>
+            <NavLink to='/purchasedbookspage' 
+            // className='linkStyle'
+                className={whenActive == 2 ? ` active` : 'normal'}
+                onClick={() => toggle(2)}
+            >
+                    <li className='nav-li'>PurchasedBooks</li>
             </NavLink>
-            <NavLink to='/bookmarkedbooksPage' className='linkStyle'>
-                 <li className={whenActive == 3 ? `isActive` : ''}
+            <NavLink to='/bookmarkedbooksPage' 
+            // className='linkStyle'
+                className={whenActive == 3 ? ` active ` : 'normal'} 
                 onClick={() => toggle(3)}
-                >Bookmarked Books</li>
+            >
+                 <li className='nav-li'>Bookmarked Books</li>
             </NavLink>
         </div>
     </div>
